862b14b91fd8a6a3866fec49aab290a712da60c0
niuhaitao5 <Ppgs8903!>
2017-11-02 15:13:13 +0800

    ‘提交''

---
+++ io/index.js
@@ -0,0 +1,17 @@
+
+var io = {
+    init: function (server) {
+        var io = require('socket.io')(server);
+        io.sockets.on('connection', function (socket) {
+            socket.on('request', function (data) {
+                socket.emit('send', {
+                    msg: '我收到的信息是' + data,
+                    user: 'admin'
+                })
+            })
+        })
+    }
+}
+
+
+module.exports = io
